5f5c1d95eab0901134ca3442e13647e66dc0c827
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-03-02 20:06:49 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,13 +1,20 @@
 from pyrogram import Client, filters
 import datetime
 import time
+import logging
 from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
-        
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
@@ -19,11 +26,15 @@ async def verupikkals(bot, message):
     done = 0
     blocked = 0
     deleted = 0
-    failed =0
+    failed = 0
 
     success = 0
     async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if 'id' not in user:
+            logging.warning(f"Skipping user {user} because 'id' key is missing.")
+            continue
+        user_id = int(user['id'])
+        pti, sh = await broadcast_messages(user_id, b_msg)
         if pti:
             success += 1
         elif pti == False:
@@ -36,7 +47,7 @@ async def verupikkals(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
@@ -51,11 +62,17 @@ async def grp_brodcst(bot, message):
     start_time = time.time()
     total_chats = await db.total_chat_count()
     done = 0
-    failed =0
+    blocked = 0
+    deleted = 0
+    failed = 0
 
     success = 0
     async for chat in chats:
-        pti, sh = await broadcast_messages(int(chat['id']), b_msg)
+        if 'id' not in chat:
+            logging.warning(f"Skipping chat {chat} because 'id' key is missing.")
+            continue
+        chat_id = int(chat['id'])
+        pti, sh = await broadcast_messages(chat_id, b_msg)
         if pti:
             success += 1
         elif pti == False:
@@ -68,6 +85,6 @@ async def grp_brodcst(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")    
+            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")
